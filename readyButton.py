4cbd4a5269999fa2e720be3d8e6bbed9a670052f
odino4ka49 <TessaMarizza@gmail.com>
2018-08-31 12:16:12 +0700

    debug color

--- readyButton.py
+++ readyButton.py
@@ -34,19 +34,35 @@ class Form(QDialog):
         self.b1 = QPushButton("e-")
         self.b1.setCheckable(True)
         self.b1.clicked.connect(lambda:self.whichbtn(self.b1))
+        self.b1.setFixedSize(100,100)
         layout.addWidget(self.b1)
 
         self.b2 = QPushButton("e+")
         self.b2.setCheckable(True)
         self.b2.clicked.connect(lambda:self.whichbtn(self.b2))
+        self.b2.setStyleSheet(
+                      "QPushButton:checked { background-color: blue;color:#000000; }" )
+        self.b2.setFixedSize(100,100)
         layout.addWidget(self.b2)
 
         self.pvname = "VEPP3:InjRequest-SP"
 
-        self.setWindowTitle("Ready")
+        self.polarity = 0
+        self.setWindowTitle("Request")
         self.setbtns(caget(self.pvname))
-        camonitor(self.pvname,self.setbtns)
 
+        camonitor(self.pvname,self.setbtns)
+        self.check_coef = 0.9
+        self.readConfig()
+        self.subscribeToCheck()
+
+
+    def subscribeToCheck(self):
+        camonitor("VEPP3:Status-RB",self.checkCurrent)
+        camonitor("VEPP3:InjRequest-SP",self.checkCurrent)
+        camonitor("VEPP3:Polarity-RB",self.checkCurrent)
+        camonitor("VEPP3:CurrentRequest-RB",self.checkCurrent)
+        camonitor("VEPP3:CurrentTotal-RB",self.checkCurrent)
 
     def setbtnzero(self,b):
         if b.isChecked():
@@ -66,11 +82,11 @@ class Form(QDialog):
                 self.setbtnzero(self.b2)
 
         else:
-            self.setPv(0.0)
+            self.setPv(0)
 
     def setbtns(self,pv_val):
         print "camonitor",pv_val
-        pv_val = caget(self.pvname)
+        self.polarity = pv_val
         if pv_val==0:
             self.setbtnzero(self.b1)
             self.setbtnzero(self.b2)
@@ -81,7 +97,6 @@ class Form(QDialog):
             self.setbtnzero(self.b1)
             self.setbtnuno(self.b2)
 
-
     def setPv(self,value):
         while True:
             try:
@@ -90,6 +105,23 @@ class Form(QDialog):
                 caput(self.pvname,float(value),wait=True)
             else:
                 break
+
+    def checkCurrent(self,value):
+        status = caget("VEPP3:Status-RB")
+        print status
+        if status == 2:
+            polarity = caget("VEPP3:Polarity-RB")
+            print polarity,self.polarity
+            if polarity == self.polarity:
+                currentreq = caget("VEPP3:CurrentRequest-RB")
+                currenttotal = caget("VEPP3:CurrentTotal-RB")
+                print currenttotal,currentreq
+                if currenttotal > currentreq*self.check_coef:
+                    self.setPv(0)
+
+    def readConfig(self):
+        with open("config") as config:
+            self.check_coef = config.readline()
 
 
 def main():
