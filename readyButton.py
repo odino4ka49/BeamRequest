bee03efc0f88752c86b2b8cc702668dbcda88cdf
odino4ka49 <TessaMarizza@gmail.com>
2018-09-04 13:01:05 +0700

    error message

--- readyButton.py
+++ readyButton.py
@@ -9,19 +9,18 @@ from cothread.catools import *
 
 
 class MonitoringThread(QThread):
-
-    def __init__(self,pvname):
+    def __init__(self, pvname):
         QThread.__init__(self)
-        self.pvname=pvname
+        self.pvname = pvname
 
     def __del__(self):
         self.wait()
 
-    def printpv(self,pv_val):
+    def printpv(self, pv_val):
         print pv_val
 
     def run(self):
-        camonitor(self.pvname,self.printpv)
+        camonitor(self.pvname, self.printpv)
 
 
 class ConfigInfo(QDialog):
@@ -38,16 +37,15 @@ class ConfigInfo(QDialog):
 
 
 class MainWin(QMainWindow):
-
     def __init__(self):
         super(MainWin, self).__init__()
         self.form = Form(self)
         self.setCentralWidget(self.form)
         self.setWindowTitle("Request")
         self.configinfo = ConfigInfo()
-        self.resize(50,50)
+        self.resize(50, 50)
 
-        helpaction = QAction("&config file",self)
+        helpaction = QAction("&config file", self)
         helpaction.triggered.connect(self.showconfig)
         mainmenu = self.menuBar()
         helpmenu = mainmenu.addMenu('&Help')
@@ -55,6 +53,9 @@ class MainWin(QMainWindow):
 
     def showconfig(self):
         self.configinfo.show()
+
+    def displayError(self,message):
+        self.err = QMessageBox.critical(self,"Error",message)
 
 
 class RequestButton(QPushButton):
@@ -65,9 +66,10 @@ class RequestButton(QPushButton):
     def setupbt(self, Text):
         self.setText(Text)
         self.setCheckable(True)
-        self.setStyleSheet('QPushButton { border-image: url(/home/oidin/projects/readybutton/ReadyButton/images/gray-rectangle-button.png);font-size:30px;}'
-                           'QPushButton:checked { border-image: url(/home/oidin/projects/readybutton/ReadyButton/images/yellow-rectangle-button.svg)}')
-        self.setFixedSize(80,80)
+        self.setStyleSheet(
+            'QPushButton { border-image: url(/home/oidin/projects/readybutton/ReadyButton/images/gray-rectangle-button.png);font-size:30px;}'
+            'QPushButton:checked { border-image: url(/home/oidin/projects/readybutton/ReadyButton/images/yellow-rectangle-button.svg)}')
+        self.setFixedSize(80, 80)
 
     def setbtnzero(self):
         if self.isChecked():
@@ -77,21 +79,22 @@ class RequestButton(QPushButton):
         if not self.isChecked():
             self.toggle()
 
+
 class Form(QWidget):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignCenter)
-	layout.setContentsMargins(0,0,0,0)
+        layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
 
         self.b1 = RequestButton("e-")
-        self.b1.clicked.connect(lambda:self.whichbtn(self.b1))
+        self.b1.clicked.connect(lambda: self.whichbtn(self.b1))
         layout.addWidget(self.b1)
 
         self.b2 = RequestButton("e+")
-        self.b2.clicked.connect(lambda:self.whichbtn(self.b2))
+        self.b2.clicked.connect(lambda: self.whichbtn(self.b2))
         layout.addWidget(self.b2)
 
         self.pvname = "VEPP3:InjRequest-SP"
@@ -99,61 +102,60 @@ class Form(QWidget):
         self.polarity = 0
         self.setbtns(caget(self.pvname))
 
-        camonitor(self.pvname,self.setbtns)
+        camonitor(self.pvname, self.setbtns)
         self.check_coef = 0.9
         self.readConfig()
         self.subscribeToCheck()
 
-
     def subscribeToCheck(self):
-        camonitor("VEPP3:Status-RB",self.checkCurrent)
-        camonitor("VEPP3:InjRequest-SP",self.checkCurrent)
-        camonitor("VEPP3:Polarity-RB",self.checkCurrent)
-        camonitor("VEPP3:CurrentRequest-RB",self.checkCurrent)
-        camonitor("VEPP3:CurrentTotal-RB",self.checkCurrent)
+        camonitor("VEPP3:Status-RB", self.checkCurrent)
+        camonitor("VEPP3:InjRequest-SP", self.checkCurrent)
+        camonitor("VEPP3:Polarity-RB", self.checkCurrent)
+        camonitor("VEPP3:CurrentRequest-RB", self.checkCurrent)
+        camonitor("VEPP3:CurrentTotal-RB", self.checkCurrent)
 
-    def whichbtn(self,b):
+    def whichbtn(self, b):
         if b.isChecked():
-            if b.text()=="e+":
+            if b.text() == "e+":
                 self.setPv(2)
                 self.b1.setbtnzero()
-            elif b.text()=="e-":
+            elif b.text() == "e-":
                 self.setPv(1)
                 self.b2.setbtnzero()
 
         else:
             self.setPv(0)
 
-    def setbtns(self,pv_val):
-        print "camonitor",pv_val
+    def setbtns(self, pv_val):
+        print "camonitor", pv_val
         self.polarity = pv_val
-        if pv_val==0:
+        if pv_val == 0:
             self.b1.setbtnzero()
             self.b2.setbtnzero()
-        elif pv_val==1:
+        elif pv_val == 1:
             self.b1.setbtnuno()
             self.b2.setbtnzero()
-        elif pv_val==2:
+        elif pv_val == 2:
             self.b1.setbtnzero()
             self.b2.setbtnuno()
 
-    def setPv(self,value):
+    def setPv(self, value):
         while True:
             try:
-                caput(self.pvname,float(value),wait=True)
+                caput(self.pvname, float(value), wait=True)
             except:
-                caput(self.pvname,float(value),wait=True)
+                caput(self.pvname, float(value), wait=True)
             else:
                 break
 
-    def checkCurrent(self,value):
+    def checkCurrent(self, value):
         status = caget("VEPP3:Status-RB")
         if status == 2:
             polarity = caget("VEPP3:Polarity-RB")
             if polarity == self.polarity:
                 currentreq = caget("VEPP3:CurrentRequest-RB")
                 currenttotal = caget("VEPP3:CurrentTotal-RB")
-                if currenttotal > currentreq*self.check_coef:
+                if currenttotal > currentreq * self.check_coef:
                     self.setPv(0)
 
     def readConfig(self):
@@ -164,8 +166,11 @@ class Form(QWidget):
 def main():
     app = cothread.iqt()
     ex = MainWin()
-    ex.show()
-    cothread.WaitForQuit()
+    try:
+        ex.show()
+        cothread.WaitForQuit()
+    except Exception as e:
+        ex.displayError(e.message)
 
 if __name__ == '__main__':
     main()
